f64f04611baf50f57b39a7dbb8cdd0a55c2c5aec
NBright24 <78198636+NBright24@users.noreply.github.com>
2021-06-01 09:45:00 +0800

    Initial Update

---
+++ Contact Tracing App/Contact Tracing App/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contact_Tracing_App
+{
+    public partial class ContactForm1 : Form
+    {
+        public ContactForm1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
